a14c8cccea3272125d263b526b1b81328fe3f88c
Mariel Goettsch <mlgoettsch@gmail.com>
2022-10-10 20:19:31 -0500

    added word list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { wordList } from './wordList';
 
 function App() {
+
+  const randomNum = Math.floor(Math.random() * wordList.length);
+  const randomWord = wordList[randomNum]
+
   return (
     <div className="App">
-      Hello world
+      Hello world {randomWord}
     </div>
   );
 }
